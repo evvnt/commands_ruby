5422e6d5b45084976243745b1570609ace92c1c2
Rhet <rhetprieto@gmail.com>
2021-04-26 22:58:59 -0600

    Added a compact_params helper

--- lib/commands/helpers.rb
+++ lib/commands/helpers.rb
@@ -1,6 +1,5 @@
 module Commands
   module Helpers
-
     #Can be used to extract certain keys from a hash.
     def pluck_from_hash(params, *keys)
       _h = {}
@@ -10,6 +9,12 @@ module Commands
 
     def _pp(params)
       params.to_unsafe_hash.deep_symbolize_keys
+    end
+
+    def compact_params(params)
+      params.map do |key, val|
+        [key, val.is_a?(Array) ? val.select(&:present?) : val]
+      end.to_h.with_indifferent_access
     end
 
   end
